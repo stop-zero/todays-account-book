6bd48c55b193841a35af4c09b4641864e20845c3
hrlee12 <gyfl5542@naver.com>
2023-03-18 16:52:02 +0900

    feat: get 경로 수정

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 5050;
 
-app.set('view engine', 'ejs');
-app.set('/views', 'views');
-app.use('/static', express.static(__dirname + '/static'));
+app.set("view engine", "ejs");
+app.set("/views", "views");
+app.use("/static", express.static(__dirname + "/static"));
 
-app.get('/', (req, res) => {
-    res.render('tutorial')
-})
+app.get("/", (req, res) => {
+  res.render("tutorial");
+});
 
-app.get('/tutorial2', (req, res) => {
-    res.render('tutorial2')
-})
+app.get("/tutorial2", (req, res) => {
+  res.render("tutorial2");
+});
 
-app.get('/testresult', (req, res) => {
-    res.render('testresult')
-})
+app.get("/tutorial2/testresult", (req, res) => {
+  res.render("testresult");
+});
 
-app.get('/main', (req, res) => {
-    res.render('main')
-})
+app.get("/main", (req, res) => {
+  res.render("main");
+});
 
 app.listen(PORT, () => {
-    console.log(`SERVER RUNNING ${PORT} !`);
-    console.log(`http://localhost:${PORT}`);
-})
+  console.log(`SERVER RUNNING ${PORT} !`);
+  console.log(`http://localhost:${PORT}`);
+});
