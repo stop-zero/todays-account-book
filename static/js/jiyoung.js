2f2ba5d14dda8add4b60004d16d2b50089bb8d9f
stop-zero <jungjy0097@naver.com>
2023-03-21 11:59:33 +0900

    tip page

--- static/js/jiyoung.js
+++ static/js/jiyoung.js
@@ -1,17 +1,16 @@
- const TrandingSlider = new Swiper('.slider', {
-    effect: 'coverflow',
-    centeredSlides: true,
-    loop: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2.5,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-   
-  });
+const Slider = new Swiper(".slider", {
+  effect: "coverflow",
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
